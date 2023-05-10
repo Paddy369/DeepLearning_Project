cc6321efba4a49c1fe5a64d00ccf93b5e9ed95f5
Finn Amini Kaveh <finnamini2@gmail.com>
2023-05-10 12:18:50 +0200

    change log structure

--- model.py
+++ model.py
@@ -1,9 +1,8 @@
-import numpy as np
 import json
 import matplotlib.pyplot as plt
 import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras import layers, Sequential, utils
+from datetime import datetime
+from tensorflow.keras import layers, Sequential, optimizers, losses, callbacks
 
 # load the model settings
 with open("config.json", 'r') as file:
@@ -18,6 +17,10 @@ train_batch_size = settings["batch_size_train"]         # batch size for trainin
 val_batch_size = settings["batch_size_val"]             # batch size for training
 test_batch_size = settings["batch_size_test"]           # batch size for training
 image_size = settings["image_size"]                     # image size
+augmentation = settings["augmentation"]                 # is augmentation enabled
+weight_decay = settings["weight_decay"]                 # is weight decay enabled
+learning_rate_decay = settings["learning_rate_decay"]   # is learning rate decay enabled
+batch_normalization = settings["batch_normalization"]   # is batch normalization enabled
 
 ################################################
 
@@ -28,7 +31,7 @@ def load_data(path, batch_size = 4):
         BASE_DIR + path,                                # path to the data directory
         labels="inferred",                              # class labels are inferred from the subdirectory structure
         label_mode="int",                               # labels are returned as integers
-        class_names=None,                               # names of the classes
+        class_names=names,                              # names of the classes
         color_mode="rgb",                               # color images
         batch_size = batch_size,                        # number of images to retrieve at a time
         image_size=(image_size, image_size),            # images are resized to 128x128
@@ -43,7 +46,8 @@ def load_data(path, batch_size = 4):
     return data
 
 # load the data
-train_batches = load_data("train", train_batch_size)    # training data is loaded in batches of 64
+train_path = "train_aug" if augmentation else "train"
+train_batches = load_data(train_path, train_batch_size) # training data is loaded in batches of 64
 val_batches = load_data("validation", val_batch_size)   # validation data is loaded in batches of 8  
 test_batches = load_data("testing", test_batch_size)    # testing data is loaded in batches of 8
 
@@ -68,35 +72,34 @@ model.add(resnet)
 model.add(layers.Flatten())
 model.add(layers.Dense(256, activation='relu'))
 model.add(layers.Dense(256, activation='relu'))
-model.add(layers.Dropout(0.1))
-model.add(layers.Dense(256, activation='softmax'))
+model.add(layers.Dropout(0.3))
+model.add(layers.Dense(15, activation='softmax'))
 
 # print the model summary
 model.summary()
 
-# compile the model
-model.compile(optimizer=keras.optimizers.Adam(learning_rate=0.001),
-              loss=keras.losses.SparseCategoricalCrossentropy(),
-              metrics=["accuracy"])
+# loop for testing out different learning rates
+for i in range(0, 1):
+    # calculate the learning rate
+    learning_rate = 10**(-i)
 
-log_dir = "logs/log" + "_epochs_" + str(epochs)
-tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
+    # compile the model
+    model.compile(optimizer=optimizers.Adam(learning_rate=learning_rate),
+                  loss=losses.SparseCategoricalCrossentropy(),
+                  metrics=["accuracy"])
 
-history = model.fit(train_batches, validation_data=val_batches, callbacks=[tensorboard_callback], epochs=epochs)
+    # path to the log directory
+    now = datetime.now()
+    log_dir = "logs/" + now.strftime("%d.%m.%Y_%H-%M-%S")
+    log_dir += "_aug_" + str(augmentation) 
+    log_dir += "_epochs_" + str(epochs) 
+    log_dir += "_lr_" + str(learning_rate)
 
-model.evaluate(test_batches)
+    # create a callback to log the data for tensorboard
+    tensorboard_callback = callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
 
-# # plot loss and acc
-# plt.figure(figsize=(16, 6))
-# plt.subplot(1, 2, 1)
-# plt.plot(history.history['loss'], label='train loss')
-# plt.plot(history.history['val_loss'], label='valid loss')
-# plt.grid()
-# plt.legend(fontsize=15)
+    # train the model
+    history = model.fit(train_batches, validation_data=val_batches, callbacks=[tensorboard_callback], epochs=epochs)
 
-# plt.subplot(1, 2, 2)
-# plt.plot(history.history['accuracy'], label='train acc')
-# plt.plot(history.history['val_accuracy'], label='valid acc')
-# plt.grid()
-# plt.legend(fontsize=15)
-# plt.show()
+    # evaluate the model
+    model.evaluate(test_batches, verbose=2)
