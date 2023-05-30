e0c2ee9b93e253c6c767c90c0993afe7c0217e95
Finn Amini Kaveh <finnamini2@gmail.com>
2023-05-30 22:00:22 +0200

    add new frontend components; display classification results in frontend

--- LiveDemo/frontend/src/components/ResultListing/index.js
+++ LiveDemo/frontend/src/components/ResultListing/index.js
@@ -1,23 +1,24 @@
 import { Card, CardMedia, CardContent } from '@mui/material'
 import styles from './styles.module.css'
 
-const ResultListing = ({ className, percentage }) => {
+const ResultListing = ({ className, percentage, isMax }) => {
+
+    console.log(className, isMax)
 
     return (
-        <Card sx={{ display: 'flex', width: 250 }} className={styles.card}>
+        <Card sx={{ display: 'flex', width: 250 }} className={`${styles.card} ${isMax && styles.max}`}>
             <CardMedia
                 component="img"
-                sx={{ width: 55 }}
-                image={`/card_thumbnails/cat.jpeg`}
+                sx={{ width: 90 }}
+                image={`/card_thumbnails/${className}.jpg`}
                 alt="Cat"
             />
-            <CardContent sx={{ flex: '1 0 auto' }}>
+            <CardContent sx={{ flex: '1 0 auto' }} className={styles.contentWrapper}>
                 <h3>{className}</h3>
                 <div>Percentage: <b>{percentage}</b></div>
             </CardContent>
         </Card>
     )
-
 }
 
 export default ResultListing
