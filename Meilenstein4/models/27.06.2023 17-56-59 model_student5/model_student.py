e25a751309f90861909470a3e46fcf8229a75549
Paddy369 <mayer.patrick97@web.de>
2023-06-28 18:36:07 +0000

    logs

---
+++ Meilenstein4/models/27.06.2023 17-56-59 model_student5/model_student.py
@@ -0,0 +1,69 @@
+import numpy as np
+import warnings
+
+from keras.layers import Input
+from keras import layers
+from keras.layers import Dense
+from keras.layers import Activation
+from keras.layers import Flatten
+from keras.layers import Conv2D
+from keras.layers import MaxPooling2D
+from keras.layers import GlobalMaxPooling2D
+from keras.layers import ZeroPadding2D
+from keras.layers import AveragePooling2D
+from keras.layers import GlobalAveragePooling2D
+from keras.layers import BatchNormalization
+from keras.layers import Dropout
+from keras.models import Model
+from keras.preprocessing import image
+import keras.backend as K
+from keras.utils import layer_utils
+
+def loadModel():
+    kernel_size = 3
+    strides = (2, 2)
+    if K.image_data_format() == 'channels_last':
+        bn_axis = 3
+    else:
+        bn_axis = 1
+        
+    img_input = Input(shape=(256, 256, 3))
+    x = ZeroPadding2D((3, 3))(img_input)
+    x = Conv2D(64, (7, 7), strides=(2, 2), name='conv1')(x)
+    x = BatchNormalization(axis=bn_axis, name='bn_conv1')(x)
+    x = Activation('relu')(x)
+    y = MaxPooling2D((3, 3), strides=(2, 2))(x)
+
+    # convolutional block
+    
+    filters1, filters2, filters3 = [128, 128, 512]    
+    x = Conv2D(filters1, (1, 1), strides=strides)(y)
+    x = BatchNormalization(axis=bn_axis)(x)
+    x = Activation('relu')(x)
+
+    x = Conv2D(filters2, kernel_size, padding='same')(x)
+    x = BatchNormalization(axis=bn_axis)(x)
+    x = Activation('relu')(x)
+
+    x = Conv2D(filters3, (1, 1))(x)
+    x = BatchNormalization(axis=bn_axis)(x)
+
+    shortcut = Conv2D(filters3, (1, 1), strides=strides)(y)
+    shortcut = BatchNormalization(axis=bn_axis)(shortcut)
+
+    x = layers.add([x, shortcut])
+    x = Activation('relu')(x)
+
+    x = AveragePooling2D((7, 7), name='avg_pool')(x)
+
+    x = Flatten() (x)
+    x = Dense(256, activation='relu', name='fc512') (x)
+    x = Dropout(0.5)(x)
+    x = Dense(128, activation='relu', name='fc256') (x)
+    x = Dropout(0.5)(x)
+
+    x = Dense(15, activation='softmax', name='fc15') (x)
+
+    model = Model(img_input, x)
+
+    return model
---
+++ Meilenstein4/models/28.06.2023 08-54-23 model_student9/model_student.py
@@ -0,0 +1,36 @@
+from tensorflow.keras import Sequential, layers
+
+def loadModel():
+    model = Sequential()
+
+    #add the layers
+    model.add(layers.BatchNormalization())
+
+    model.add(layers.Conv2D(16, (3, 3), activation='relu', padding='same'))
+    model.add(layers.Conv2D(16, (3, 3), activation='relu', padding='same'))
+    model.add(layers.Conv2D(32, (3, 3), activation='relu', padding='same'))
+    model.add(layers.MaxPooling2D((2, 2), strides=(2,2)))
+
+    model.add(layers.BatchNormalization())
+
+    model.add(layers.Conv2D(32, (3, 3), activation='relu', padding='same'))
+    model.add(layers.Conv2D(32, (3, 3), activation='relu', padding='same'))
+    model.add(layers.Conv2D(64, (3, 3), activation='relu', padding='same'))
+    model.add(layers.MaxPooling2D((2, 2), strides=(2,2)))
+
+    model.add(layers.BatchNormalization())
+
+    model.add(layers.Conv2D(64, (3, 3), activation='relu', padding='same'))
+    model.add(layers.Conv2D(64, (3, 3), activation='relu', padding='same'))
+    model.add(layers.Conv2D(128, (3, 3), activation='relu', padding='same'))
+    model.add(layers.MaxPooling2D((2, 2), strides=(2,2)))
+
+    #flatten the output
+    model.add(layers.Flatten()) 
+    
+    #add the dense layers
+    model.add(layers.Dense(256, activation='relu'))
+    model.add(layers.Dropout(0.4, seed=1))
+    model.add(layers.Dense(128, activation='relu'))
+    model.add(layers.Dense(15, activation='softmax'))    
+    return model
